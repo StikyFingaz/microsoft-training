c39cb6ef16d9bd4b58db7b858c93e853101ea493
Peter Bozukov <bozukov.p@gmail.com>
2024-07-02 11:58:07 +0300

    Inital commit

---
+++ BuildDotNetApps/DotNetFiles/Program.cs
@@ -0,0 +1,13 @@
+﻿IEnumerable<string> listOfDirs = Directory.EnumerateDirectories(".");
+
+// foreach (var item in listOfDirs)
+// {
+//     Console.WriteLine($"{item}");
+// }
+
+IEnumerable<string> files = Directory.EnumerateFiles("./obj");
+
+foreach (var file in files)
+{
+    Console.WriteLine($"{file}");
+}
---
+++ BuildDotNetApps/mslearn-dotnet-files/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+
+var currentDir = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDir, "stores");
+
+var salesTotalDir = Path.Combine(currentDir, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);
+
+var salesFiles = FindSales(storesDirectory);
+var salesTotal = CalculteSalesTotal(salesFiles);
+
+// if (!File.Exists(Path.Combine(salesTotalDir, "totals.txt")))
+// {
+//     File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), string.Empty);
+// }
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+IEnumerable<string> FindSales(string folderName)
+{
+    // List<string> salesFiles = [];
+    // var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    // foreach (var file in foundFiles)
+    // {
+    //     if (file.EndsWith("sales.json"))
+    //     {
+    //         salesFiles.Add(file);
+    //     }
+    // }
+
+    return Directory
+        .EnumerateFiles(folderName, "*", SearchOption.AllDirectories)
+        .Where(file => Path.GetExtension(file) == ".json");
+}
+
+double CalculteSalesTotal(IEnumerable<string> salesFiels)
+{
+    double salesTotal = 0;
+
+    foreach (var file in salesFiels)
+    {
+        string salesJson = File.ReadAllText(file);
+        SalesData? data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData(double Total);
